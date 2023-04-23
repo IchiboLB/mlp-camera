752b11222df7f31376a5436dc02b8cebd388a248
DimaPuz2010 <75927089+IchiboLB@users.noreply.github.com>
2023-04-23 15:31:40 +0600

    camera

--- MLPcamera/app/src/main/java/com/basdoor/mlpcamera/camera/ScamActivity.kt
+++ MLPcamera/app/src/main/java/com/basdoor/mlpcamera/camera/ScamActivity.kt
@@ -3,11 +3,22 @@ package com.basdoor.mlpcamera.camera
 import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.basdoor.mlpcamera.R
+import com.google.common.util.concurrent.ListenableFuture
 
-class ScamActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+class ScamActivity : AppCompatActivity() {
+    private lateinit var camProvider: ListenableFuture<ProcessCameraProvider>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scam)
+        camProvider = ProcessCameraProvider.getInstance(this)
+        camProvider.addListener({
+
+
+
+        }, ContextCompat.getMainExecutor(this))
     }
 }
--- MLPcamera/app/src/main/java/com/basdoor/mlpcamera/scanstart/StartFragment.kt
+++ MLPcamera/app/src/main/java/com/basdoor/mlpcamera/scanstart/StartFragment.kt
@@ -1,14 +1,34 @@
 package com.basdoor.mlpcamera.scanstart
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.basdoor.mlpcamera.R
+import com.basdoor.mlpcamera.camera.ScamActivity
 
 class StartFragment : Fragment() {
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+
+    ) { isGranted:Boolean ->
+        if (isGranted == true) {
+            startScam()
+        } else {
+            //domashnee zadanie: pri otkaze scrivat start scan knopku i pokazat soobschenie
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,9 +41,18 @@ class StartFragment : Fragment() {
         
         super.onViewCreated(view, savedInstanceState)
         val startButton = view.findViewById<Button>(R.id.start)
+        //startButton.isVisible (k dz)
         startButton.setOnClickListener {
-
+        val permission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+        when(permission) {
+            PackageManager.PERMISSION_GRANTED -> {startScam()}
+            else -> {permissionLauncher.launch(Manifest.permission.CAMERA)}
         }
+        }
+    }
+        private fun startScam() {
+            val scamIntent = Intent(requireContext(), ScamActivity:: class.java)
+            startActivity(scamIntent)
     }
 
 }
